33ec43dd1b3906f7b2d9d390940f62451350d087
Donald Curtis <dcurtis@milkbox.net>
2013-04-04 20:28:27 -0500

    install cl-lib if it doesn't exist in travis build

--- run-travis-ci.sh
+++ run-travis-ci.sh
@@ -9,4 +9,6 @@ echo "ECUKES_EMACS = $ECUKES_EMACS"
 "$ECUKES_EMACS" --version
 echo
 
+"$ECUKES_EMACS" --batch --no-site-file --eval "(or (ignore-errors (require 'cl-lib)) (package-install 'cl-lib))"
+
 exec ./util/ecukes/ecukes --graphical
